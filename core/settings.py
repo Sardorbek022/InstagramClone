beae74d5aaae9701b29cf0720ff425d4bcc28651
SardorDev <sardordev022@gmail.com>
2024-08-15 21:25:06 +0500

    add User and UserConfirmation model

--- core/settings.py
+++ core/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     # installed apps
     'jazzmin',
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -33,6 +34,7 @@ INSTALLED_APPS = [
 
     # created apps
     'users',
+    'shared',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
