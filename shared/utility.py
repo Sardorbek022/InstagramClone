e757ece96708887ac7c151f78ec472003b1279c0
SardorDev <sardordev022@gmail.com>
2024-08-29 14:49:34 +0500

    add reset password

--- shared/utility.py
+++ shared/utility.py
@@ -9,6 +9,7 @@ from rest_framework.exceptions import ValidationError
 
 email_regex = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b")
 phone_regex = re.compile(r"(\+[0-9]+\s*)?(\([0-9]+\))?[\s0-9\-]+[0-9]+")
+username_regex = re.compile(r"^[a-zA-Z0-9_.-]+$")
 
 def check_email_or_phone(email_or_phone):
     if re.fullmatch(email_regex, email_or_phone):
@@ -25,6 +26,24 @@ def check_email_or_phone(email_or_phone):
         raise ValidationError(data)
 
     return email_or_phone
+
+
+def check_user_type(user_input):
+    # phone_number = phonenumbers.parse(user_input)
+    if re.fullmatch(email_regex, user_input):
+        user_input = 'email'
+    elif re.fullmatch(phone_regex, user_input):
+        user_input = 'phone'
+    elif re.fullmatch(username_regex, user_input):
+        user_input = 'username'
+    else:
+        data = {
+            "success": False,
+            "message": "Email, username yoki telefon raqamingiz noto'g'ri"
+        }
+        raise ValidationError(data)
+    return user_input
+
 
 class EmailThread(threading.Thread):
 
@@ -60,5 +79,16 @@ def send_email(email, code):
             "body": html_content,
             "content_type": "html"
         }
+    )
+
+
+def send_phone_code(phone, code):
+    account_sid = config('account_sid')
+    auth_token = config('auth_token')
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        body=f"Salom do'stim! Sizning tasdiqlash kodingiz: {code}\n",
+        from_="+99899325242",
+        to=f"{phone}"
     )
 
